18e0b50a95d61e21e048a5e3a9d9704252a875e0
Vijay K.C <97362857+kcvijay@users.noreply.github.com>
2022-02-28 14:28:53 +0200

    Update script.js
    There is a significant change in the whole JavaScript file in terms of making it more dynamic. If a user chooses between feet & inches or centimeters for height selection, input fields for other buttons disappear. And, results for BMI is made more simpler to understand.

--- BMI CALCULATOR/script.js
+++ BMI CALCULATOR/script.js
@@ -1,17 +1,87 @@
 "use strict";
 
-document.getElementById("calcBmi").addEventListener("click", function () {
-  let weight = Number(document.getElementById("weight").value);
-  let height = Number(document.getElementById("height").value);
-  let resultText = document.getElementById("result");
+let inputMetric = document.querySelector(".box--1");
+let inputImperial = document.querySelector(".box--2");
 
-  let bmi = weight / (((height / 100) * height) / 100);
+let inputHeight = Number(document.getElementById("height").value);
+let inputWeight = Number(document.getElementById("weight").value);
 
-  console.log(height);
-  console.log(weight);
-  console.log(bmi.toFixed(2));
-  if (weight > 0 && height > 0) {
-    if (bmi < 15 && bmi > 19) {
+let inputFeet = Number(document.getElementById("feet").value);
+let inputInch = Number(document.getElementById("inch").value);
+
+const btnMetric = document.getElementById("metric");
+const btnImperial = document.getElementById("imperial");
+
+let resultText = document.getElementById("result");
+
+const btnReset = document.getElementById("reset");
+
+btnImperial.addEventListener("click", function () {
+  inputMetric.classList.add("hidden");
+  inputImperial.classList.remove("hidden");
+  btnMetric.style.backgroundColor = "lightgray";
+  btnImperial.style.backgroundColor = "green";
+  document.getElementById("weight").value = "";
+  document.getElementById("height").value = "";
+  resultText.textContent = "";
+});
+
+btnMetric.addEventListener("click", function () {
+  inputImperial.classList.add("hidden");
+  inputMetric.classList.remove("hidden");
+  btnImperial.style.backgroundColor = "lightgray";
+  btnMetric.style.backgroundColor = "green";
+  document.getElementById("weight").value = "";
+  document.getElementById("inch").value = "";
+  document.getElementById("feet").value = "";
+  resultText.textContent = "";
+});
+
+btnImperial.addEventListener("click", function () {
+  document.getElementById("calcBmi").addEventListener("click", function () {
+    let inputWeight = Number(document.getElementById("weight").value);
+    let resultText = document.getElementById("result");
+
+    let inputFeet = Number(document.getElementById("feet").value);
+    let inputInch = Number(document.getElementById("inch").value);
+    let bmiImperial =
+      inputWeight /
+      ((inputFeet * 0.3048 + inputInch * 0.0254) *
+        (inputFeet * 0.3048 + inputInch * 0.0254));
+
+    console.log(bmiImperial);
+    if (bmiImperial > 15 && bmiImperial < 19) {
+      resultText.textContent = `Your BMI is ${bmiImperial.toFixed(
+        1
+      )} and underweight.`;
+    } else if (bmiImperial > 19 && bmiImperial < 25) {
+      resultText.textContent = `Your BMI is ${bmiImperial.toFixed(
+        1
+      )} and normal.`;
+    } else if (bmiImperial > 25 && bmiImperial < 30) {
+      resultText.textContent = `Your BMI is ${bmiImperial.toFixed(
+        1
+      )} and mildly overweight.`;
+    } else if (bmiImperial > 30) {
+      resultText.textContent = `Your BMI is ${bmiImperial.toFixed(
+        1
+      )} and critically overweight.`;
+    } else {
+      resultText.textContent = `Data input invalid.`;
+    }
+  });
+});
+
+btnMetric.addEventListener("click", function () {
+  document.getElementById("calcBmi").addEventListener("click", function () {
+    let inputWeight = Number(document.getElementById("weight").value);
+    let inputHeight = Number(document.getElementById("height").value);
+
+    let resultText = document.getElementById("result");
+
+    let bmi = inputWeight / (((inputHeight / 100) * inputHeight) / 100);
+
+    if (bmi > 15 && bmi < 19) {
       resultText.textContent = `Your BMI is ${bmi.toFixed(1)} and underweight.`;
     } else if (bmi > 19 && bmi < 25) {
       resultText.textContent = `Your BMI is ${bmi.toFixed(1)} and normal.`;
@@ -26,11 +96,13 @@ document.getElementById("calcBmi").addEventListener("click", function () {
     } else {
       resultText.textContent = `Data input invalid.`;
     }
-  }
+  });
 });
 
-document.getElementById("reset").addEventListener("click", function () {
-  weight.value = "";
-  height.value = "";
-  resultText.textContent = "";
+btnReset.addEventListener("click", function () {
+  document.getElementById("height").value = "";
+  document.getElementById("weight").value = "";
+  document.getElementById("feet").value = "";
+  idocument.getElementById("inch").value = "";
+  document.getElementById("result").textContent = "";
 });
